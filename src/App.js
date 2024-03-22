24740b41d7c2f353f5af5d7809229e57495ce681
Arnabg_9 <arnabghorai15@gmail.com>
2024-03-22 22:36:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Hari Ram",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Krishna",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Jogonnath",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+  {
+    id: 499476,
+    name: "Jogonnath",
+    image: "https://i.pravatar.cc/48?u=499477",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const [splitClick, setSplitClick] = useState(false);
+  const [showAddForm, setShowAddForm] = useState(false);
+  function handleSplitClick() {
+    setSplitClick((splitClick) => !splitClick);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={splitClick ? `app2` : `app`}>
+      {!splitClick ? <Logo onSplitClick={handleSplitClick} /> : null}
+      {splitClick && (
+        <>
+          <div className="left">
+            <div className="icon-container">
+              <i class="ri-arrow-left-line" onClick={handleSplitClick}></i>
+            </div>
+            <UserList />
+            {showAddForm && <AddUserForm />}
+            <div className="button">
+              <button
+                onClick={() => setShowAddForm((showAddForm) => !showAddForm)}
+              >
+                {showAddForm ? "Close" : "Add"}
+              </button>
+            </div>
+          </div>
+          <div className="right"></div>
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+function Logo({ onSplitClick }) {
+  return (
+    <div className="logo" onClick={onSplitClick}>
+      {initialFriends.map((friend) => (
+        <LogoAvater friend={friend} />
+      ))}
+    </div>
+  );
+}
+function LogoAvater({ friend }) {
+  return <img src={friend.image} alt={friend.name} />;
+}
+
+function UserList() {
+  return (
+    <ul className="user-list">
+      {initialFriends.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </ul>
+  );
+}
+
+function User({ user }) {
+  return (
+    <li>
+      <img src={user.image} alt={user.name} />
+      <div className="user-info">
+        <p>{user.name}</p>
+        <p>
+          {user.balance > 0
+            ? `${user.name} owe you $ ${user.balance}`
+            : `you owe ${user.name} $ ${user.balance * -1}`}
+        </p>
+      </div>
+      <i class="ri-add-line"></i>
+      <i class="ri-edit-box-line"></i>
+      <i class="ri-delete-bin-6-line"></i>
+    </li>
+  );
+}
+
+function AddUserForm() {
+  return (
+    <div className="user-add-form">
+      <form>
+        <div>
+          <label>Enter Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Enter imageURL</label>
+          <input type="text" />
+        </div>
+        <div className="btn">
+          <button>Add</button>
+        </div>
+      </form>
+    </div>
+  );
+}
